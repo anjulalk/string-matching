4e008ae32ff6ac729ffe59a555f26b49b1d5a68c
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-06-11 16:29:46 +0530

    Added DNA query matching functioanlity:

--- question2/app.py
+++ question2/app.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python3
 def main():
-	t = 'aabcdefabcdef'
-	p = 'def'
-	print(kmpMatch(t,p))
+    dnaDBFile = open('DNA Database.txt', 'r')
+    dnaDB = {}
+
+    #populate dnaDB dictionary from dnaDBFile text file
+    for line in dnaDBFile:
+        global desc
+        if line[0]=='>':
+            desc = line.strip('>\n')
+            if desc != 'EOF':
+                dnaDB[desc] = ''
+        else:
+            dnaDB[desc] = dnaDB[desc] + line.strip('\n ')
+    
+    queryDBFile = open('querybase.txt', 'r')
+    queryDB = {}
+    #populate queryDB dictionary from queryDBFile text file
+    for line in queryDBFile:
+        global qdesc
+        if line[0]=='>':
+            qdesc = line.strip('>\n')
+            if qdesc != 'EOF':
+                queryDB[qdesc] = ''
+        else:
+            queryDB[qdesc] = queryDB[qdesc] + line.strip('\n ')
+    
+    for a,b in dnaDB.items():
+        print(a)
+        for m,n in queryDB.items():
+            matches = kmpMatch(b, n)
+            if len(matches)==0:
+                print('NOT FOUND')
+            else:
+                print('[' + n + '] at offset ', end='')
+                for match in matches:
+                    print(match, end=' ')
+                print()
 
 def kmpMatch(t, p):
 	n = len(t)
@@ -18,7 +51,6 @@ def kmpMatch(t, p):
 		if k==m:
 			matches.append(i-(k-1))
 			k = prefix[k-1]
-	print(t,p, matches)	
 	return matches
 
 def prefixFn(p):
