1bcc6e9639dd69dfa54646df3241e4d471aa5693
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-06-12 02:26:05 +0530

    Added more test data

--- question1/app.py
+++ question1/app.py
@@ -1,36 +1,54 @@
 #!/usr/bin/env python3
 import sys
 
+# Gets text, pattern and output file names as arguments
+# Exit if missing arguments
 def main(argv):
+	# Check if all arguments exist
 	if len(argv) != 4:
 		print("ERROR: Missing command line arguments!")
 		print("Usage: ./app.py text_file.txt pattern_file.txt output_file.txt")
 		return 1
+	
+	# Try to open the files
 	try:
-		textFile = open(argv[1], 'r')
-		patternFile = open(argv[2], 'r')
+		textFile, patternFile = open(argv[1], 'r'), open(argv[2], 'r')
 	except IOError:
 		print("ERROR: Missing input files!")
 		return 2
+	
+	# Try to open output file
+	# Exit if doesn't have permission to write
 	try:
 		outputFile = open(argv[3], 'w+')
 	except IOError:
 		print("ERROR: Couldn't write into output file!")
 		return 3
+	
+	# Run the function to genereate outputFile
 	patternMatch(textFile, patternFile, outputFile)
 
-def patternMatch(textFile, patternFile, outputFile):
-	text, pattern = textFile.readline().strip('\n '), patternFile.readline().strip('\n ')
-	text_length, pattern_length = len(text), len(pattern)
-	for i in range(text_length-pattern_length+1):
-		j = 0
-		for j in range(pattern_length):
-			if text[i+j] != pattern[j] and pattern[j] != '_':
-				j -= 1
-				break
-		if (j == pattern_length-1):
-			outputFile.write('{} '.format(i))
+	# Close opened files
+	textFile.close()
+	patternFile.close()
 	outputFile.close()
+
+def patternMatch(textFile, patternFile, outputFile):
+	# Get the contents of text and pattern files to respective string variables
+	text, pattern = textFile.readline().strip('\n '), patternFile.readline().strip('\n ')
+	
+	text_length, pattern_length = len(text), len(pattern)
+	
+	# Iterate over the text
+	for i in range(text_length-pattern_length+1):
+		j = 0 # j is the position of the pattern which is matched
+		for j in range(pattern_length): # Iterate over the pattern
+			if text[i+j] != pattern[j] and pattern[j] != '_': 
+				# MISMATCH!
+				j -= 1 # Decrement j by one so the current character is ignored
+				break
+		if (j == pattern_length-1): # Checks if pattern is complete
+			outputFile.write('{} '.format(i))
 
 if __name__=='__main__':
 	main(sys.argv)
--- question1/test.py
+++ question1/test.py
@@ -3,27 +3,62 @@ import app
 
 def main():
     testPassCount = 0
-    testCount = 1
-    textFiles = ['./testdata/pattern1.txt']
-    patternFiles =['./testdata/text1.txt']
-    outputFiles = ['./testdata/patternmatch1.output']
-    expectedOutputs = ['']
+
+    # Number of test cases
+    testCount = 10
+
+    # List of text files
+    textFilePaths = ['./testdata/text1.txt', './testdata/text2.txt', './testdata/text3.txt', './testdata/text4.txt', './testdata/text5.txt', './testdata/text6.txt', './testdata/text7.txt', './testdata/text8.txt', './testdata/text9.txt', './testdata/text10.txt']
+    
+    # List of pattern files
+    patternFilePaths =['./testdata/pattern1.txt', './testdata/pattern2.txt', './testdata/pattern3.txt', './testdata/pattern4.txt', './testdata/pattern5.txt', './testdata/pattern6.txt', './testdata/pattern7.txt', './testdata/pattern8.txt', './testdata/pattern9.txt', './testdata/pattern10.txt']
+    
+    # List of output files
+    outputFilePaths = ['./testdata/patternmatch1.output', './testdata/patternmatch2.output', './testdata/patternmatch3.output', './testdata/patternmatch4.output', './testdata/patternmatch5.output', './testdata/patternmatch6.output', './testdata/patternmatch7.output', './testdata/patternmatch8.output', './testdata/patternmatch9.output', './testdata/patternmatch10.output']
+    
+    # List of expected/correct outputs for the respective files
+    expectedOutputs = ['0 8', '29', '153', '9', '', '', '', '', '', '', ]
+
     for i in range(testCount):
-        print('Test: {} - Text:{} Pattern:{} Output:{}'.format(i, textFiles[i], patternFiles[i], outputFiles[i]))
-        app.patternMatch(open(textFiles[i], 'r'), open(patternFiles[i], 'r'), open(outputFiles[i], 'w+'))
-        print('Expected output: {}'.format(expectedOutputs[i]))
-        #try:
-            #outputFile = open(outputFiles[i], 'r')
-            #returnedOutput = outputFile.readline().strip('\n ')
-            #print('Returned output: {}'.format(returnedOutput))
-            #outputFile.close()
-        #except IOError:
-            #print("ERROR: Couldn't read output file!")
-        #if (expectedOutputs[i]==returnedOutput):
-            #print('PASS')
-            #testPassCount += 1
-        #else:
-            #print('FAIL')
+        # Retrieve respective file paths from the lists
+        textFilePath, patternFilePath, outputFilePath = textFilePaths[i], patternFilePaths[i], outputFilePaths[i]
+        
+        # Open respective files
+        textFile, patternFile, outputFile = open(textFilePath, 'r'), open(patternFilePath, 'r'), open(outputFilePath, 'w+')
+        
+        print('TEST {}\nText: {}\tPattern: {}\tOutput: {}'.format(i, textFilePath, patternFilePath, outputFilePath))
+        
+        # Generate output
+        app.patternMatch(textFile, patternFile, outputFile)
+        
+        # Close files
+        textFile.close()
+        patternFile.close()
+        outputFile.close()
+
+        # Retrieve output file content and check if it matches expected output
+        try:
+            outputFile = open(outputFilePaths[i], 'r')
+            output = outputFile.readline().strip('\n ')
+            print('Output: {}'.format(output))
+            outputFile.close()
+        except IOError:
+            print("ERROR: Couldn't read output file!")
+        
+        print('Expected: {}'.format(expectedOutputs[i]))
+        
+        if (expectedOutputs[i]==output):
+            print('PASS')
+            testPassCount += 1
+        else:
+            print('FAIL')
+        
+        # Close files
+        textFile.close()
+        patternFile.close()
+        outputFile.close()
+    
+    # Display final result
     print("\nTESTS: {}/{} ({}%)".format(testPassCount, testCount, testPassCount/testCount*100))
 
 if __name__=='__main__':
