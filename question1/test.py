06b484a29bdaec68281d0d3d98d2323d01460584
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-06-12 01:16:45 +0530

    Added test cases

--- question1/test.py
+++ question1/test.py
@@ -3,27 +3,28 @@ import app
 
 def main():
     testPassCount = 0
-    testCount = 2
-    textFiles = []
-    patternFiles =[]
-    outputFiles = []
-    expectedOutputs = []
+    testCount = 1
+    textFiles = ['./testdata/pattern1.txt']
+    patternFiles =['./testdata/text1.txt']
+    outputFiles = ['./testdata/patternmatch1.output']
+    expectedOutputs = ['']
     for i in range(testCount):
         print('Test: {} - Text:{} Pattern:{} Output:{}'.format(i, textFiles[i], patternFiles[i], outputFiles[i]))
-        app.patternMatch(textFiles[i], patternFiles[i], outputFiles[i])
+        app.patternMatch(open(textFiles[i], 'r'), open(patternFiles[i], 'r'), open(outputFiles[i], 'w+'))
         print('Expected output: {}'.format(expectedOutputs[i]))
-        try:
-            outputFile = open(outputFiles, 'r')
-            returnedOutput = outputFile.readline()
-            print('Returned output: {}'.format(returnedOutput))
-        except IOError:
-            print("ERROR: Couldn't write into output file!")
-        if (expectedOutputs[i]==returnedOutput):
-            print('PASS')
-            testPassCount += 1
-        else:
-            print('FAIL')
-    print("\nTESTS: {}/{}".format(testPassCount, testCount))
+        #try:
+            #outputFile = open(outputFiles[i], 'r')
+            #returnedOutput = outputFile.readline().strip('\n ')
+            #print('Returned output: {}'.format(returnedOutput))
+            #outputFile.close()
+        #except IOError:
+            #print("ERROR: Couldn't read output file!")
+        #if (expectedOutputs[i]==returnedOutput):
+            #print('PASS')
+            #testPassCount += 1
+        #else:
+            #print('FAIL')
+    print("\nTESTS: {}/{} ({}%)".format(testPassCount, testCount, testPassCount/testCount*100))
 
 if __name__=='__main__':
     main()
