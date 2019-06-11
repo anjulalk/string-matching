92d886fa9cf5b70939c6ec5b8db17dbf1c3ffdea
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-06-11 21:36:25 +0530

    Patch Naive algorithm to skip over wildcard

--- question1/app.py
+++ question1/app.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 def main():
-	t = 'aabcdefabcd'
-	p = 'aac'
+	t = 'abx'
+	p = 'a_b'
 	
+	n, m = len(t), len(p)
+	for i in range(n-m+1):
+		for j in range(0, m):
+			if t[i+j] != p[j] and p[j] != '_':
+				break
+		if (j==m-1):
+			print('found at', i)
+
+
 
 if __name__=='__main__':
 	main()
