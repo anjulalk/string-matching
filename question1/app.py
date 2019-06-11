23f3a18e93404d9ceec79603694fbd95d1e5d2ab
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-06-11 23:35:54 +0530

    Modify program to accept files from command line arguments

--- question1/app.py
+++ question1/app.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
-def main():
-	t = 'abx'
-	p = 'ab_'
+import sys
+
+def main(argv):
+	if len(argv) != 4:
+		print("ERROR: Missing command line arguments!")
+		print("Usage: ./app.py text_file.txt pattern_file.txt output_file.txt")
+		return 1
+	try:
+		textFile = open(argv[1], 'r')
+		patternFile = open(argv[2], 'r')
+	except IOError:
+		print("ERROR: Missing input files!")
+		return 2
+	try:
+		outputFile = open(argv[3], 'w+')
+	except IOError:
+		print("ERROR: Couldn't write into output file!")
+		return 3
 	
-	n, m = len(t), len(p)
-	for i in range(n-m+1):
-		for j in range(0, m):
-			if t[i+j] != p[j] and p[j] != '_':
+	text, pattern = textFile.readline().strip('\n '), patternFile.readline().strip('\n ')
+	text_length, pattern_length = len(text), len(pattern)
+	#n, m = len(t), len(p)
+	for i in range(text_length-pattern_length+1):
+		j = 0
+		for j in range(pattern_length):
+			if text[i+j] != pattern[j] and pattern[j] != '_':
 				j -= 1
 				break
-		if (j == m-1):
-			print('found at', i)
+		if (j == pattern_length-1):
+			outputFile.write('{} '.format(i))
 
 if __name__=='__main__':
-	main()
+	main(sys.argv)
