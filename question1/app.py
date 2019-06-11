3cb870a4cfe627bcce8bd071e4614490478807b5
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-06-11 21:59:46 +0530

    Fixed a bug where texts like abc matches with a_b

--- question1/app.py
+++ question1/app.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 def main():
 	t = 'abx'
-	p = 'a_b'
+	p = 'ab_'
 	
 	n, m = len(t), len(p)
 	for i in range(n-m+1):
 		for j in range(0, m):
 			if t[i+j] != p[j] and p[j] != '_':
+				j -= 1
 				break
-		if (j==m-1):
+		if (j == m-1):
 			print('found at', i)
-
-
 
 if __name__=='__main__':
 	main()
